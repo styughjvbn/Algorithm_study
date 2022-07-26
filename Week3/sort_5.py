831d58a081f800a64e5fd624fdeb8b32d92b7d98
JongWon <3338761@naver.com>
2022-07-26 18:53:57 +0900

    하루 1문제 이상 풀기로 다시시작.

--- Week3/sort_5.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-n = sys.stdin.readline().strip()
-
-print("".join((s for s in sorted(n,reverse=True))))
--- Week5,6/11051.py
+++
@@ -1,6 +0,0 @@
-import sys
-import math
-
-[num1,num2]=list(map(int,sys.stdin.readline().strip().split()))
-
-print(math.comb(num1,num2)%10007)
