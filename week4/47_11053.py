361a1928163e2019ab23b9f83f12a4f031592d18
JongWon <3338761@naver.com>
2022-08-17 16:31:10 +0900

    23일차

---
+++ week4/47_11053.py
@@ -0,0 +1,15 @@
+import sys
+
+n=int(sys.stdin.readline().strip())
+arr=list(map(int,sys.stdin.readline().split()))
+
+dp=[1]*n
+
+for i in range(n):
+    max1=0
+    for j in range(i):
+        if arr[j]<arr[i] and dp[j]>max1:
+            max1=dp[j]
+    dp[i]=max1+1
+
+print(max(dp))
