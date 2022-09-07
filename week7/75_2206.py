0edb907e3d041a4ccbb49b9a22df1ae0b1cf4afa
JongWon <3338761@naver.com>
2022-09-07 23:21:55 +0900

    44일차

---
+++ week7/75_2206.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+#미로를 표현한 2차원 배열을 visit로 같이 사용하려고 해서 해당 탐색 순서의 전단계에서 벽을 뚫었는지를 제대로 분별하지 못했다.
+#visti 배열을 3차원으로 따로 만들어서 큐에 넣고 전단계에서 벽을 뚫었는지를 큐에서 가져온 원소에서 알수있다.
+input = sys.stdin.readline
+dx = [1, -1, 0, 0]
+dy = [0, 0, -1, 1]
+def bfs():
+    q = deque()
+    q.append([0, 0, 1])
+    visit = [[[0] * 2 for i in range(m)] for i in range(n)]
+    visit[0][0][1] = 1
+    while q:
+        a, b, w = q.popleft()
+        if a == n - 1 and b == m - 1:
+            return visit[a][b][w]
+        for i in range(4):
+            x = a + dx[i]
+            y = b + dy[i]
+            if 0 <= x < n and 0 <= y < m:
+                if s[x][y] == 1 and w == 1:
+                    visit[x][y][0] = visit[a][b][1] + 1
+                    q.append([x, y, 0])
+                elif s[x][y] == 0 and visit[x][y][w] == 0:
+                    visit[x][y][w] = visit[a][b][w] + 1
+                    q.append([x, y, w])
+    return -1
+n, m = map(int, input().split())
+s = []
+for i in range(n):
+    s.append(list(map(int, list(input().strip()))))
+print(bfs())
