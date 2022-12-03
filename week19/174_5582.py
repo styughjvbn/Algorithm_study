d1ea7dab8e3092943b54b6a545e49703f0ffd6b3
JongWon <3338761@naver.com>
2022-12-03 13:45:54 +0900

    131일차

---
+++ week19/174_5582.py
@@ -0,0 +1,14 @@
+import sys
+input=sys.stdin.readline
+
+str2=input().strip()
+str1=input().strip()
+dp=[[0 for _ in range(len(str2)+1)]for _ in range(len(str1)+1)]
+result=0
+for i in range(1,len(str1)+1):
+    for j in range(1,len(str2)+1):
+        if str1[i-1]==str2[j-1]:
+            dp[i][j]=dp[i-1][j-1]+1
+            result=max((dp[i][j]),result)
+
+print(result)
