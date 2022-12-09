2db9f3e673376c396e4c135774eca88994d2f6ec
JongWon <3338761@naver.com>
2022-12-09 18:31:51 +0900

    137일차

---
+++ week20/180_17609.py
@@ -0,0 +1,35 @@
+import sys
+
+def ispseudo(word, left, right):
+    while left < right:
+        if word[left] == word[right]:
+            left += 1
+            right -= 1
+        else:
+            return False
+    return True
+
+def ispalindrome(word, left, right):
+    if word == word[::-1]:
+        return 0
+    else:
+        while left < right:
+            if word[left] != word[right]:
+                check_left = ispseudo(word, left + 1, right)
+                check_right = ispseudo(word, left, right - 1)
+
+                if check_left or check_right:
+                    return 1
+                else:
+                    return 2
+            else:
+                left += 1
+                right -= 1
+
+T = int(sys.stdin.readline().rstrip("\n"))
+
+for _ in range(T):
+    word = sys.stdin.readline().rstrip("\n")
+    left, right = 0, len(word)-1
+    answer = ispalindrome(word, left, right)
+    print(answer)
