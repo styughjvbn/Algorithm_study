91a3f38c08afa23c43e097758a290afd7b67984e
JongWon <3338761@naver.com>
2023-01-30 11:36:52 +0900

    188일차 - 그리디

---
+++ week28/234_15903.py
@@ -0,0 +1,15 @@
+import sys,heapq
+
+input=sys.stdin.readline
+
+n,m=map(int,input().split())
+heap=list(map(int,input().split()))
+heapq.heapify(heap)
+
+for i in range(m):
+    num1=heapq.heappop(heap)
+    num2 = heapq.heappop(heap)
+    heapq.heappush(heap,num1+num2)
+    heapq.heappush(heap, num1 + num2)
+
+print(sum(heap))
