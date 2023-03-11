9e832721d613d0002719d364a0b74784ae39dd9b
JongWon <3338761@naver.com>
2023-03-11 22:40:00 +0900

    225일차 - 그래프

--- week33/272_1922.py
+++ week33/272_1922.py
@@ -1,8 +1,8 @@
 import sys
-
 input=sys.stdin.readline
 
 def find(x):
+    #유니온 파인드 알고리즘의 집합의 루트를 찾는 함수
     if computer[x]<0:
         return x
     else:
@@ -10,6 +10,7 @@ def find(x):
         return computer[x]
 
 def union(x,y):
+    #유니온 파인드 알고리즘의 원소들을 하나의 집합으로 합치는 함수
     root_x=find(x)
     root_y=find(y)
     if root_x==root_y:
@@ -25,13 +26,18 @@ def union(x,y):
 
 N=int(input())
 M=int(input())
+
+#유니온 파인드의 각 집합을 표현한다. 처음에는 컴퓨터 개수만큼의 원소가 1개씩인 집합이 N개 존재한다.
 computer=[-1 for _ in range(N)]
+
+#컴퓨터끼리의 경로을 입력받고 비용을 기준으로 오름차순 정렬한다.
 line=[]
 for i in range(M):
     a,b,c=map(int,input().split())
     line.append([a,b,c])
 line.sort(key=lambda a:a[2])
 
+#Kruskal MST 알고리즘에 따라 비용이 작고 집합에 존재하지 않는 경로를 선택한다.
 result=0
 for i in range(M):
     a,b,c=line[i]
