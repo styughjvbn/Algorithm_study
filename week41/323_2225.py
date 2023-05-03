789e0baf6f32d9760b6bfbf769f701e04c70e0b5
JongWon <3338761@naver.com>
2023-05-03 18:07:28 +0900

    276일차 - DP

---
+++ week41/323_2225.py
@@ -0,0 +1,7 @@
+n,k=map(int,input().split())
+
+dp=[[1 for _ in range(n+1)]for _ in range(k)]
+for i in range(1,k):
+    for j in range(1,n+1):
+        dp[i][j]=dp[i-1][j]+dp[i][j-1]
+print(dp[k-1][n]%1000000000)
