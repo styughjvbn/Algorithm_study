12738a79ea196fe15eac73a568f524ef572363be
JongWon <3338761@naver.com>
2023-06-27 00:09:55 +0900

    305일차 - 브루트포스

--- week41-50/week49/359_2210.py
+++ week41-50/week49/359_2210.py
@@ -1,25 +1,26 @@
-def dfs(x,y,number):
-  if len(number)==6:
-    if number not in result:
-      result.append(number)
-    return
-
-  dx=[1,-1,0,0]
-  dy=[0,0,1,-1]
-  for i in range(4):
-    nx=x+dx[i]
-    ny=y+dy[i]
-    if nx<0 or nx>=5 or ny<0 or ny>=5:
-      continue
-    else:
-      dfs(nx,ny,number+board[nx][ny])
-
-board=[]
-for i in range(5):
-  board.append(list(map(str, input().split())))
+num_board=[list(map(str,input().split())) for _ in range(5)]
 
 result=[]
-for i in range(5):
-  for j in range(5):
-    dfs(i,j,board[i][j])
+di=[0,1,0,-1]
+dj=[1,0,-1,0]
+def dfs(string,i,j):
+    if len(string)==6:
+        if int(string) in result:
+            return
+        else:
+            result.append(int(string))
+        return
+    else:
+        for k in range(4):
+            ni=i+di[k]
+            nj=j+dj[k]
+            if ni<0 or ni>=5 or nj<0 or nj>=5:
+                continue
+            else:
+                dfs(string+num_board[ni][nj],ni,nj)
+
+for p in range(5):
+    for l in range(5):
+        dfs(num_board[p][l],p,l)
+
 print(len(result))
