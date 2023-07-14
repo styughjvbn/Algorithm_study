8cd9092c31ae586f4b45765572c9daca5a3c368a
styughjvbn <3338761@naver.com>
2023-07-14 22:40:32 +0900

    317일차 - 풀어본 문제 다시 풀어보기 11

---
+++ week51-60/week51/382_1238.py
@@ -0,0 +1,45 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+INF = int(1e9)
+
+v, e, x = map(int, input().split())
+graph = [[] for _ in range(v + 1)]
+
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    graph[a].append((b, cost))
+
+
+def dijkstra(start):
+    q = []
+    distance = [INF] * (v + 1)
+
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+
+        #다른 노드를 거치면서 더 빠른 길을 이미 구했으므로 생략
+        if distance[now] < dist:
+            continue
+
+        for node_index, node_cost in graph[now]:
+            cost = dist + node_cost
+
+            if distance[node_index] > cost:
+                distance[node_index] = cost
+                heapq.heappush(q, (cost, node_index))
+
+    return distance
+
+
+result = 0
+for i in range(1, v + 1):
+    go = dijkstra(i)
+    back = dijkstra(x)
+    result = max(result, go[x] + back[i])
+
+print(result)
